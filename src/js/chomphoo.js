e1cb41f5b9976b87655da4e4ff8295e2fa3fc400
Chomphoo <chomphoo@protonmail.com>
2022-10-30 19:15:12 +0100

    update selectReview && submit function

--- src/js/chomphoo.js
+++ src/js/chomphoo.js
@@ -1,7 +1,15 @@
-function selectReview(id){
-    console.log(document.getElementById(id).id + " is selected");
+function selectReview(reviewId){
+    let nbReview = 5;
+
+    for (id = 1 ; id <= nbReview ; id++){ //remove all class "selected" to review button
+        document.getElementById(id).classList.remove("selected");
+    }
+
+    document.getElementById(reviewId).classList.add("selected") //add class "selected"
 }
 
 function submit(){
-    console.log("Hello World !")
+    //Default if not selected: NA
+    let reviewSelected = (document.getElementsByClassName("selected")[0] != undefined ? document.getElementsByClassName("selected")[0].id : "NA");
+    console.log("review " + reviewSelected + " is selected");
 }
